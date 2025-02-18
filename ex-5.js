a9f31f925ba2d6fd0b4f61ee110a72d8626b53ab
fluke <chaikhwan32@gmail.com>
2025-02-18 09:37:30 +0700

    JavaScript Primitive Values Assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalProductPrice = 27000 ;
+let discout25percen = (25*totalProductPrice)/100 ;
+let totalMustPay = totalProductPrice-discout25percen ;
+
+console.log(totalMustPay);
